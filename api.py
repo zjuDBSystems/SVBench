71ec3a2fc7f9a9d1d6c858659653c62ddd4185dd
DDDDDstar <2643622564@qq.com>
2024-10-20 13:24:36 +0800

    [FEAT] Add api.py to provide external api
    api.py provides a function `SV_calc` for users to calculate SV for their
    DA task. By `from api import SV_calc`, users can call the function with
    parameters: the number of player, the function to calculate the
    utility implemented by users and other parameters related to SV
    calculation.

---
+++ api.py
@@ -0,0 +1,33 @@
+from SV import Shapley
+import numpy as np
+
+
+# default arguments
+args = {
+    # SV parameters
+    'num_parallelThreads': 1,
+    'base_compFunc': 'MC',
+    'convergence_threshold': 0.5,
+    'scannedIter_maxNum': np.inf,
+    'MLE_maxInterval': 10000,
+    'GT_epsilon': 0.00001,
+    'num_measurement': 10,
+    'CP_epsilon': 0.00001,
+    'sampling_strategy': 'random',
+    'truncation': False,
+    'truncationThreshold': 0.01,
+    'gradient_approximation': False,
+    'testSampleSkip': False,
+
+    # SV's privacy protection parameters
+    'privacy_protection_measure': None,
+    'privacy_protection_level': 0.0,
+}
+
+
+def SV_calc(player_num, taskUtilityFunc, **kwargs):
+    for key, value in kwargs.items():
+        setattr(args, key, value)
+
+    shap = Shapley(player_num, taskUtilityFunc, args)
+    shap.CalSV()
