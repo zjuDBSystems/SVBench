ce964d7c14e0ad7ede45d2d86923565e30c3b298
Hoho Lin <43038310+Hoho-Lin@users.noreply.github.com>
2025-04-05 09:14:28 +0800

    Update exp.py (change the default convergence threshold to 0.05)

--- exp.py
+++ exp.py
@@ -28,7 +28,7 @@ def args_parser():
                         help="SV cache_size")
     parser.add_argument('--base_algo', type=str, default="MC",
                         help="{MC, RE, MLE, GT, CP}")
-    parser.add_argument('--convergence_threshold', type=float, default=0.01,
+    parser.add_argument('--convergence_threshold', type=float, default=0.05,
                         help="approximation convergence_threshold")
     parser.add_argument('--checker_mode', type=str, default='SV_var',
                         help="checker_mode: SV_var or comp_count")
