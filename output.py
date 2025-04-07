ce8dfe9237e0a76d2c7378b11747b376b044ed98
Hoho Lin <43038310+Hoho-Lin@users.noreply.github.com>
2025-04-07 09:25:33 +0800

    Update output.py

--- output.py
+++ output.py
@@ -101,7 +101,7 @@ class Aggregator():
             self.A_CP = 1 / np.sqrt(self.num_measurement) * \
                 (2 * np.random.binomial(
                     1, 0.5, size=(self.num_measurement, self.player_num)) - 1)
-            self.CP_epsilon = 0.005
+            self.CP_epsilon = 0.05
 
     def aggregate(self, results, task_total_utility, task_emptySet_utility):
         if self.algo == 'MC':
